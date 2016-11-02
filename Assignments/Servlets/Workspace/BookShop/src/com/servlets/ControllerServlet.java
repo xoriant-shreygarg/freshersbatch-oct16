76e20883c711c118a22bafbc19a104a16d3c6b25
Shrey Garg <Garg_s@India.XoriantCorp.com>
2016-11-02 18:22:43 +0530

    2nd nov servlets

---
+++ Assignments/Servlets/Workspace/BookShop/src/com/servlets/ControllerServlet.java
@@ -0,0 +1,38 @@
+package com.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/ControllerServlet")
+public class ControllerServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("uname");
+		HttpSession session = request.getSession(true);
+		if (session.isNew())
+			session.setAttribute("uname", username);
+		String forward = request.getParameter("forward");
+		RequestDispatcher rd = null;
+		if (forward.equals("login"))
+			rd = request.getRequestDispatcher("LoginServlet");
+		else if (forward.equals("addBooks"))
+			rd = request.getRequestDispatcher("AddBooksServlet");
+		else if (forward.equals("shoppingCart"))
+			rd = request.getRequestDispatcher("ShoppingCartServlet");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
